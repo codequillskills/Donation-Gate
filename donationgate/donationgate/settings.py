dedaf7895b67ad6f27756380589ab0e0060463d8
Shivam Gupta <codequillskills@gmail.com>
2024-08-14 05:02:02 +0530

    Update settings.py

--- donationgate/donationgate/settings.py
+++ donationgate/donationgate/settings.py
@@ -80,11 +80,9 @@ WSGI_APPLICATION = 'donationgate.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
